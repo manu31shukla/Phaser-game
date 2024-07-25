87ad3da765ce6627097113de8ec0f21eda276471
manu31shukla <manu31shukla@gmail.com>
2024-07-25 19:47:55 +0530

    created obstacle bars and bouce back

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -8,10 +8,16 @@ class Game extends Phaser.Scene{
         const ball = 
         this.add.circle(400, 250, 10, 0xffffff,  1);
         this.physics.add.existing(ball);
+        ball.body.setBounce(1,1);
 
         ball.body.setCollideWorldBounds(true,1, 1);
+        ball.body.setVelocity(-200, 0);
 
-        ball.body.setVelocity(200, 200);
+        const paddleLeft = this.add.rectangle(50, 250, 10, 100, 0xffffff);
+        this.physics.add.existing(paddleLeft, true);
+        // paddleLeft.body.setBounce(1,1);
+
+        this.physics.add.collider(ball, paddleLeft);
     }
 }
     
