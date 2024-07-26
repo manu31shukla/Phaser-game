4105e4d76d62c37fd090ab7dff0c62d2d96adaf9
manu31shukla <manu31shukla@gmail.com>
2024-07-26 16:30:30 +0530

    ball movements non-linear andstarted  right paddle movements

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,19 +5,22 @@ class Game extends Phaser.Scene{
 
     }
     create(){
-        const ball = 
+        this.ball = 
         this.add.circle(400, 250, 10, 0xffffff,  1);
-        this.physics.add.existing(ball);
-        ball.body.setBounce(1,1);
+        this.physics.add.existing(this.ball);
+        this.ball.body.setBounce(1,1);
 
-        ball.body.setCollideWorldBounds(true,1, 1);
-        ball.body.setVelocity(-200, 0);
+        this.ball.body.setCollideWorldBounds(true,1, 1);
+        this.ball.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-200, 200)); ;
 
-        this.paddleLeft = this.add.rectangle(50, 250, 10, 100, 0xffffff);
+        this.paddleLeft = this.add.rectangle(50, 250, 20, 100, 0xffffff);
         this.physics.add.existing(this.paddleLeft, true);
         // paddleLeft.body.setBounce(1,1);
+        this.physics.add.collider(this.ball, this.paddleLeft);
 
-        this.physics.add.collider(ball, this.paddleLeft);
+        this.paddleRight= this.add.rectangle(750, 250, 30, 100, 0xffffff, 1);
+        this.physics.add.existing(this.paddleRight, true);
+        this.physics.add.collider(this.ball, this.paddleRight);
 
         this.cursors = this.input.keyboard.createCursorKeys() 
     }
@@ -31,6 +34,17 @@ class Game extends Phaser.Scene{
             this.paddleLeft.y += 10;     
             this.paddleLeft.body.updateFromGameObject();  
         } 
+        const diff = this.ball.y - this.paddleRight.y;
+        if(diff<10){
+            //ball is below paddle  
+            this.paddleRight.y -= 10;
+            this.paddleRight.body.updateFromGameObject();
+        }
+        else if(diff>10){
+            //ball is above paddle
+            this.paddleRight.y += 10;
+            this.paddleRight.body.updateFromGameObject();
+        }
     }
 }
     
