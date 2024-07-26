01a415f3854f5d0f99abea67d57dc84a5789909f
manu31shukla <manu31shukla@gmail.com>
2024-07-26 15:55:53 +0530

    added movements for bar using up-down cursor movements

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -13,11 +13,25 @@ class Game extends Phaser.Scene{
         ball.body.setCollideWorldBounds(true,1, 1);
         ball.body.setVelocity(-200, 0);
 
-        const paddleLeft = this.add.rectangle(50, 250, 10, 100, 0xffffff);
-        this.physics.add.existing(paddleLeft, true);
+        this.paddleLeft = this.add.rectangle(50, 250, 10, 100, 0xffffff);
+        this.physics.add.existing(this.paddleLeft, true);
         // paddleLeft.body.setBounce(1,1);
 
-        this.physics.add.collider(ball, paddleLeft);
+        this.physics.add.collider(ball, this.paddleLeft);
+
+        this.cursors = this.input.keyboard.createCursorKeys() 
+    }
+
+    update(){
+        // /** @type {Phaser.Physics.Arcade.StaticBody} */
+        // const body = this.paddleLeft.body;
+
+        if(this.cursors.up.isDown){
+            this.paddleLeft.y -= 10;     
+            console.log("up");   }
+        else if(this.cursors.down.isDown){
+            this.paddleLeft.y += 10;        
+            console.log("down");   }
     }
 }
     
